762c84ccce4f7c72f60e86652a0fe66f17269bb6
Cristian Peralta <cristianeph@me.com>
2021-10-20 21:56:00 -0500

    Unit test

--- src/test/java/com/tech/userapi/UserApiApplicationTests.java
+++ src/test/java/com/tech/userapi/UserApiApplicationTests.java
@@ -1,13 +1,50 @@
 package com.tech.userapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tech.userapi.controller.request.PhoneRequest;
+import com.tech.userapi.controller.request.UserRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UserApiApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		this.mockMvc.perform(
+				post("/api/user")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(asJsonString(UserRequest.builder()
+						.name("test")
+						.email("test@test.com")
+						.password("22aaiIO$$")
+						.phones(Arrays.asList(PhoneRequest.builder()
+								.phone("123")
+								.cityCode("1")
+								.countryCode("2")
+								.build()))
+						.build()))
+		).andExpect(status().isCreated());
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
