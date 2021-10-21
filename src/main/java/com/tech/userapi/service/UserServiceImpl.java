cec608413925c91dbd51d7e41b511e54e1edb8e0
Cristian Peralta <cristianeph@me.com>
2021-10-20 21:56:25 -0500

    Code cleaning

--- src/main/java/com/tech/userapi/service/UserServiceImpl.java
+++ src/main/java/com/tech/userapi/service/UserServiceImpl.java
@@ -9,15 +9,12 @@ import com.tech.userapi.repository.models.User;
 import com.tech.userapi.util.JwtUtils;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @Slf4j
